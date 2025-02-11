dd85ac793ef6088e6c0c5b39cb0825b351e06ef0
Karthik Asadi <155179825+asadikarthik@users.noreply.github.com>
2025-02-11 19:32:14 +0530

    Time: 0 ms (100%), Space: 44.8 MB (57.92%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -4,14 +4,14 @@ class Solution {
         int j=0;
         for(int i=0; i<n;i++)
         {
-            arr[j++]=nums[i];
-            j++;
+            arr[j]=nums[i];
+            j=j+2;
         }
         j=1;
         for(int i=n; i<2*n;i++)
         {
-            arr[j++]=nums[i];
-            j++;
+            arr[j]=nums[i];
+            j=j+2;
         }
         return arr;
     }
