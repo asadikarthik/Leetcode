abdf6bf16e5992c0adc3ac2e9416c9a3d08a684a
Karthik Asadi <155179825+asadikarthik@users.noreply.github.com>
2025-08-03 23:27:44 +0530

    Time: 250 ms (5.27%), Space: 46.2 MB (6.98%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public boolean isPalindrome(String s)
+    {
+         if(s.isEmpty())return true;
+        
+        
+        String result="";
+        for(int i=0;i<s.length();i++)
+        {
+            char x = s.charAt(i);
+            //System.out.println((int)x);
+            if( (int)x>=65 && (int)x <=90 )
+            {
+                result+=s.charAt(i);
+            }
+            else if( (int)x>=97 && (int)x <=122)
+            {
+                result+=s.charAt(i);
+            }
+            else if(Character.isDigit(s.charAt(i)))
+            {
+                result+=s.charAt(i);
+            }
+        }
+        String k  = result.toLowerCase();
+        //System.out.println(k);
+        String rev="";
+        for(int i=k.length()-1;i>=0;i--)
+        {
+            rev+= k.charAt(i);
+        }
+        //System.out.println(rev);
+        if(k.compareTo(rev) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
