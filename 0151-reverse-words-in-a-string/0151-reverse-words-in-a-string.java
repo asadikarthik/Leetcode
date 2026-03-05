a76befb3055ba1ba7128339229d63cb40f334065
Karthik Asadi <155179825+asadikarthik@users.noreply.github.com>
2026-03-06 01:41:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,5 +1,21 @@
 class Solution {
     public String reverseWords(String s) {
-        
+         
+        String arr[]= s.split(" ");
+        StringBuilder sb =new StringBuilder();
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i].length()==0){
+                continue;
+            }
+            if(sb.length()==0){
+                sb.append(arr[i]);
+            }
+            else{
+                sb.append(" ");
+                sb.append(arr[i].trim());
+            }
+        }
+
+        return sb.toString();
     }
 }
