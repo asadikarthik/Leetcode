f1f775146c417f6ea2e9da5fd9a9f1bd43204051
Karthik Asadi <155179825+asadikarthik@users.noreply.github.com>
2026-03-15 01:39:20 +0530

    Time: 1 ms (100%), Space: 43.4 MB (74.81%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxPower(String s) {
+        if(s.length()==1) return 1;
+        int count=1;
+        int max=1;
+
+        for(int i=0;i<s.length()-1;i++)
+        {
+            if(s.charAt(i)==s.charAt(i+1))
+            {
+                count++;
+            }
+            else
+            {
+                count=1;
+            }
+            max = Math.max(max, count);
+        }
+
+        return max;
+    }
+}
