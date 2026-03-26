9e96fc5d44b190bd1003be129b518ca286baff2d
Karthik Asadi <155179825+asadikarthik@users.noreply.github.com>
2026-03-26 23:00:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,35 +9,66 @@
  * }
  */
 class Solution {
+    public ListNode middle(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+
+        return slow;
+    }
+
+    public ListNode merge(ListNode head)
+    {
+        if(head==null || head.next==null)
+        {
+            return head;
+        }
+        ListNode mid = middle(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next=null;
+        left= merge(left);
+        right=merge(right);
+        
+
+        return mergerTwoLists(left,right);
+    }
+
+    public ListNode mergerTwoLists(ListNode l1, ListNode l2)
+    {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while(l1 != null && l2 != null)
+        {
+            if(l1.val <= l2.val)
+            {
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+
+        if(l1 != null) temp.next = l1;
+        if(l2 != null) temp.next = l2;
+
+        return dummy.next;
+    }
+
     public ListNode sortList(ListNode head) {
         
-        ArrayList<Integer> al = new ArrayList<>();
-
-        ListNode l = head;
-
-        while(l!=null)
-        {
-            al.add(l.val);
-            l=l.next;
-        }
-        Collections.sort(al);
-        // System.out.println(al);
-
-        if(al.size()==0)
-        {
-            return null;
-        }
-        else
-        {
-            ListNode s = new ListNode(al.get(0));
-            ListNode t = s;
-            for(int i=1;i<al.size();i++)
-            {
-                ListNode u = new ListNode(al.get(i));
-                t.next = u;
-                t=u;
-            }
-            return s;
-        }
+        return merge(head);
     }
+
 }
